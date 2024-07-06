c80705ec9c01474e9bfa9fb2bffad5461b6844d8
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-07-06 17:34:15 +0300

    Keybd event version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use windows::Win32::UI::Input::KeyboardAndMouse::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
 fn main() {
+    let mut key_pressed: bool = false;
     loop {
         let alt_gr_pressed: bool; // 0xA5
         let w_pressed: bool; // 0x57
@@ -11,9 +13,21 @@ fn main() {
             x_pressed = is_button_pressed(0x58);
         };
 
-        println!("Alt Gr pressed: {alt_gr_pressed}, W pressed {w_pressed}, X pressed {x_pressed}");
+        println!("Alt Gr pressed: {alt_gr_pressed}, W pressed {w_pressed}, X pressed {x_pressed}, state of var {key_pressed}");
+        if alt_gr_pressed && w_pressed && x_pressed && !key_pressed{
+            unsafe { 
+                keybd_event(0x41, 0, KEYBD_EVENT_FLAGS(0), 0);
+                keybd_event(0x41, 0, KEYBD_EVENT_FLAGS(2), 0);
+                key_pressed = true;
+            };
+        }
+
+        if !alt_gr_pressed && !w_pressed && !x_pressed {
+            key_pressed = false;
+        }
     }
 }
+
 
 unsafe fn is_button_pressed(vkey_code: i32) -> bool {
     let press_result: i16 = GetAsyncKeyState(vkey_code);
