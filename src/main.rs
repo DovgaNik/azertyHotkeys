3506f4112225a0a40f854c618113f62f48ac7842
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-07-06 16:22:45 +0300

    Implemented reading of the keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use windows::Win32::UI::Input::KeyboardAndMouse::*;
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let alt_gr_pressed: bool; // 0xA5
+        let w_pressed: bool; // 0x57
+        let x_pressed: bool; // 0x58
+
+        unsafe { 
+            alt_gr_pressed = is_button_pressed(0xA5);
+            w_pressed = is_button_pressed(0x57);
+            x_pressed = is_button_pressed(0x58);
+        };
+
+        println!("Alt Gr pressed: {alt_gr_pressed}, W pressed {w_pressed}, X pressed {x_pressed}");
+    }
+}
+
+unsafe fn is_button_pressed(vkey_code: i32) -> bool {
+    let press_result: i16 = GetAsyncKeyState(vkey_code);
+
+    if press_result != 0 {
+        return true;
+    } else {
+        return false;
+    }
 }
